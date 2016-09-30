0389e88eeb017deddd81e962e30744ff92629a52
陈锐 <chenrui_accp@163.com>
2016-09-30 13:50:59 +0800

    添加client-win

---
+++ apitest-client-win/src/main/java/com/luckyrui/apitest/apitest/client/win/App.java
@@ -0,0 +1,16 @@
+package com.luckyrui.apitest.apitest.client.win;
+
+import com.luckyrui.apitest.client.cmd.ApiTestMain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApiTestMain ap = new ApiTestMain();
+        
+    }
+}
